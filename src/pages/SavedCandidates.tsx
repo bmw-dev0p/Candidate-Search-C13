5622b8ccd58d73d46d015e13e7aa959f0cddb2a5
bmw-dev0p <bmw.dev0p@gmail.com>
2024-09-10 22:20:47 -0400

    fixed import bug

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import type Candidate from '../interfaces/Candidate.interface';
 import '../index.css';
 
